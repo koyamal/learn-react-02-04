0b934b917ba0db4010d2c51f1835989e51ae9897
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-30 09:54:21 +0900

    Route to nested pages

---
+++ src/Page1DetailA.jsx
@@ -0,0 +1,8 @@
+export const Page1DetailA = () => {
+  return (
+    <div>
+      <h1>Page 1!</h1>
+      <h2>DetailA</h2>
+    </div>
+  );
+};
---
+++ src/Page1DetailB.jsx
@@ -0,0 +1,8 @@
+export const Page1DetailB = () => {
+  return (
+    <div>
+      <h1>Page 1!</h1>
+      <h2>DetailB</h2>
+    </div>
+  );
+};
